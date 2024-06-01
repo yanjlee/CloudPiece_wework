909efdf54a7c5a0ece11e623621668a89f721081
shadow <yanjlee@163.com>
2024-06-01 18:30:35 +0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Name    : setup.py
+# @Author  : yanlee
+
+import setuptools
+import shutil
+
+# 删除dist/目录
+shutil.rmtree('dist', ignore_errors=True)
+
+setuptools.setup(
+    name="CloudPiece_wework",
+    version="3.2.1",
+    author="yanjlee",
+    author_email="yanjlee@163.com",
+    description="根据 JoysKang 的 https://github.com/JoysKang/CloudPiece ，修改而来。使用企业微信的自建应用接收消息。目前适配了文本消息，其他类型的消息因需要服务器转存后才能推送到notion，暂时不支持",  # 模块简介
+    install_requires=[
+        'requests',
+        'faker',
+        'execjs',
+        'loguru',
+        'base64',
+        'hashlib',
+        'Crypto',
+        'pandas',
+        'fuzzywuzzy',
+        'httpx',
+        'Pillow',
+        'playwright',
+        'PyExecJS',
+        'redis',
+        'fastapi',
+        'uvicorn',
+        'APScheduler',
+        'beautifulsoup4',
+        'bs4',
+        'certifi',
+        'clickhouse-driver',
+        'curl-cffi',
+        'DrissionPage',
+        'fake-useragent',
+        'Flask',
+        'Flask-APScheduler',
+        'Flask-Cors',
+        'frida',
+        'gevent',
+        'httpx',
+        'Jinja2',
+        'langchain',
+        'langchain-community',
+        'suiutils-py',
+    ],
+    long_description=open(r'readme.md', encoding='utf-8').read(),  # 读取readme自述文件
+    long_description_content_type="text/markdown",
+    url="https://github.com/yanjlee/CloudPiece_wework",  # 模块github地址
+    packages=setuptools.find_packages(),     # 自动列出项目下的包
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",   # 开源许可证
+        "Operating System :: OS Independent",      # 这里的定义是系统无关（全平台兼容），如果你的包只能在部分特定系统上运行，需要修改。
+    ],
+)
